1194c9f7d787d799d71a97b1d4d767c3b146b313
nandor-poka <np@bio-bio.info>
2020-04-08 09:36:17 +0200

    working with EMBL-Tools at JL root

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,58 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { requestAPI } from './embl-tools-jl';
+//import { requestAPI } from './embl-tools-jl';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { ILauncher } from '@jupyterlab/launcher';
 
+import emb_logo from '../style/EMBL_logo.svg';
+
+const FACTORY = 'Notebook';
+const CATEGORY = 'EMBL Tools';
+
+namespace CommandIDs {
+  export const clustalo = 'emlb-tools-jl:clustalo';
+}
 /**
  * Initialization data for the embl-tools-jl extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'embl-tools-jl',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension embl-tools-jl is activated!');
-
-    requestAPI<any>('get_example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The embl_tools_jl server extension appears to be missing.\n${reason}`
-        );
+  requires: [ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    launcher: ILauncher
+    ) => {
+      const { commands } = app;
+      const clustalo = CommandIDs.clustalo;
+      const icon = new LabIcon({
+        name: 'launcher:clustalo',
+        svgstr: emb_logo
       });
-  }
+      console.log('JupyterLab extension embl-tools-jl is activated!');
+
+      commands.addCommand(clustalo, {
+        label: 'ClustalO',
+        caption: 'ClustalO webservice',
+        icon: icon,
+        execute: async => {
+          return commands.execute('docmanager:open', {
+            path: 'EMBL-Tools/clustalo.ipynb',
+            factory: FACTORY
+          });
+        }
+      });  
+     
+      // Add the clustalo to the launcher
+      if (launcher) {
+        launcher.add({
+          command: clustalo,
+          category: CATEGORY,
+          rank: 1
+        });
+      } 
+    }
 };
 
 export default extension;
