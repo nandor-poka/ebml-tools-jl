70a926a1f289a437537f50862b328738fe1d7657
nandor-poka <np@np-bio.info>
2020-04-14 18:18:26 +0200

    added menu for tools and WIP for settings for email and default output save path

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import { requestAPI } from './embl-tools-jl';
 import { LabIcon } from '@jupyterlab/ui-components';
 import { ILauncher } from '@jupyterlab/launcher';
 import { PathExt } from '@jupyterlab/coreutils';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { IMainMenu } from '@jupyterlab/mainmenu';
+import { Menu } from '@lumino/widgets';
 
 import embLogo from '../style/EMBL_logo.svg';
 
 const FACTORY = 'Notebook';
 const CATEGORY = 'EMBL Tools';
+const PLUGIN_ID = 'embl-tools-jl:launcher-icons';
 
 const toolNameMapping = new Map<string, string>();
 toolNameMapping.set('clustalo.ipynb', 'ClustalO');
@@ -21,19 +25,57 @@ toolNameMapping.set('ncbiBlast.ipynb', 'NCBI BLAST');
  * Initialization data for the embl-tools-jl extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'embl-tools-jl',
+  id: PLUGIN_ID,
   autoStart: true,
-  requires: [ILauncher],
-  activate: async (app: JupyterFrontEnd, launcher: ILauncher) => {
+  requires: [ILauncher, ISettingRegistry, IMainMenu],
+  activate: async (
+    app: JupyterFrontEnd,
+    launcher: ILauncher,
+    settings: ISettingRegistry,
+    mainMenu: IMainMenu
+  ) => {
     const { commands } = app;
     const commandPrefix = 'embl-tools-jl:';
     const icon = new LabIcon({
-      name: 'launcher:clustalo',
+      name: 'launcher:embl-icon',
       svgstr: embLogo
     });
     let foundExtension = false;
     let emblToolsPath = '';
     console.log('JupyterLab extension embl-tools-jl is activated!');
+    /**
+     * Load the settings for this extension
+     *
+     * @param setting Extension settings
+     */
+    function loadSetting(setting: ISettingRegistry.ISettings): void {
+      // Read the settings and convert to the correct type
+      const email = setting.get('email').composite as string;
+      const outdir = setting.get('outdir').composite as string;
+
+      console.log(`Settings are  set to '${email}' and flag to '${outdir}'`);
+    }
+
+    // Wait for the application to be restored and
+    // for the settings for this plugin to be loaded
+    Promise.all([app.restored, settings.load(PLUGIN_ID)])
+      .then(([, setting]) => {
+        // Read the settings
+        loadSetting(setting);
+
+        // Listen for your plugin setting changes using Signal
+        setting.changed.connect(loadSetting);
+
+        // Create a menu
+        //const settingsMenu = new Menu({ commands });
+        //settingsMenu.title.label = 'EMBL-Tools Settings';
+        //mainMenu.addMenu(settingsMenu, { rank: 80 });
+      })
+      .catch(reason => {
+        console.error(
+          `Something went wrong when reading the settings.\n${reason}`
+        );
+      });
     // GET request
     try {
       const data = await requestAPI<any>('startup');
@@ -46,6 +88,18 @@ const extension: JupyterFrontEndPlugin<void> = {
     }
 
     if (foundExtension) {
+      // Create a menu in the main menu bar
+      const toolsMainMenu = new Menu({ commands });
+      toolsMainMenu.title.label = 'EMBL-Tools';
+      mainMenu.addMenu(toolsMainMenu, { rank: 80 });
+      // Create submenu for the tool's own menu for tools.
+      const toolsSubMenu = new Menu({ commands });
+      toolsSubMenu.title.label = 'Tools';
+      toolsMainMenu.addItem({type:'submenu',submenu: toolsSubMenu});
+
+      const settingsSubMenu = new Menu({ commands });
+      settingsSubMenu.title.label = 'Settings';
+      toolsMainMenu.addItem({type:'submenu',submenu: settingsSubMenu});
       let tools: string[] = [];
       try {
         const data = await requestAPI<any>('toolcheck', {
@@ -79,17 +133,25 @@ const extension: JupyterFrontEndPlugin<void> = {
             }
           }
         );
-        // Add the clustalo to the launcher
-        if (launcher) {
-          launcher.add({
-            command: commandPrefix + tools[index].toLowerCase().split('.')[0],
-            category: CATEGORY,
-            rank: rank
-          });
-        }
+
+        // Add the current tool to the launcher
+        launcher.add({
+          command: commandPrefix + tools[index].toLowerCase().split('.')[0],
+          category: CATEGORY,
+          rank: rank
+        });
+        toolsSubMenu.addItem({
+          command: commandPrefix + tools[index].toLowerCase().split('.')[0],
+          
+        });
+        
         ++rank;
       }
+
+
     }
+
+
   }
 };
 
