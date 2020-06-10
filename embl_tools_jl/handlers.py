d1e43339aa558b7dbc6290777b7b7bb739452f8f
np <np@gerhardt.io>
2020-06-10 17:02:20 +0200

    moved logfile out from server extension folder to /home/np/private/.embl-tools/ where the settings are

--- embl_tools_jl/handlers.py
+++ embl_tools_jl/handlers.py
@@ -6,16 +6,22 @@ import tornado
 import os
 import logging
 import subprocess
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-__log_file_path = os.path.join(__location__, 'embl-tools-jl.log')
-__embl_tools_location__ = os.path.join(__location__,'../embl_tools/')
-_settings_file_dir_ = os.path.join(os.environ['HOME'],'private/.embl_tools')
-_settings_file_location_ = os.path.join(_settings_file_dir_, 'settings.json')
+_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+_embl_tools_location = os.path.join(_location,'../embl_tools/')
+_private_dir = os.path.join(os.environ['HOME'],'private/.embl_tools')
+_log_file_path = os.path.join(_private_dir, 'embl-tools-jl.log')
+_settings_file_location = os.path.join(_private_dir, 'settings.json')
 _settings = None
+try:
+    if not os.path.exists(_private_dir):
+        os.makedirs(_private_dir)
+except Exception as ex:
+    print("Failed to create directory for private data.")
+    
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-logFileHandler = logging.FileHandler(__log_file_path, mode='a', encoding='utf-8', delay=False)
+logFileHandler = logging.FileHandler(_log_file_path, mode='a', encoding='utf-8', delay=False)
 logFileHandler.setFormatter(formatter)
 logger.addHandler(logFileHandler)
 logger.propagate=False
@@ -61,34 +67,34 @@ class Startup_handler(APIHandler):
             logger.info(f'EMBL-Tools not found in current Jupyter Lab root dir: {self.root_dir}. Copying built in version as temporary solution.')
             real_embl_path = os.path.join(self.root_dir,"embl_tools")
             embl_path = os.path.relpath('embl_tools', self.root_dir)
-            logger.info(f'Executing: cp -R {__embl_tools_location__} {real_embl_path})')
+            logger.info(f'Executing: cp -R {_embl_tools_location} {real_embl_path})')
             try:
-                subprocess.run(['cp', '-R', f'{__embl_tools_location__}', f'{real_embl_path}'])
+                subprocess.run(['cp', '-R', f'{_embl_tools_location}', f'{real_embl_path}'])
                 logger.info('Copying done.')
             except Exception as ex:
                 logger.error(f'Failed to copy EMBL-tools to current Jupyer Lab location\n{ex}')
             logger.info("Setting PYTHONPATH environmental variable.")
             os.environ['PYTHONPATH']=f'{os.path.abspath(embl_path)}{os.pathsep}$PYTHONPATH'
             
-            if not os.path.exists(_settings_file_location_):
-                if not os.path.exists(_settings_file_dir_):
+            if not os.path.exists(_settings_file_location):
+                if not os.path.exists(_private_dir):
                     logger.info(f'Creating parent directory for settings file: {_settings_file_dir_}')                   
                     try:
-                        os.makedirs(_settings_file_dir_)
+                        os.makedirs(_private_dir)
                         logger.info(f'{_settings_file_dir_} created.') 
                     except Exception as ex:
                         logger.error(f'Failed to create parent directory for settings file at: {_settings_file_dir_}\n{ex}')
                 try:
-                    logger.info(f'Settings file not found at: {_settings_file_location_}\nCreating empty settings file.')
-                    with open(_settings_file_location_, mode='w') as settingsFile:
+                    logger.info(f'Settings file not found at: {_settings_file_location}\nCreating empty settings file.')
+                    with open(_settings_file_location, mode='w') as settingsFile:
                             settingsFile.write(json.dumps({}))
                             settingsFile.close()
                 except Exception as ex:
                     logger.error(f'Failed create empty settings file.\n{ex}')
             else:
-                logger.info(f'Settings found at: {_settings_file_location_}')
+                logger.info(f'Settings found at: {_settings_file_location}')
             try:
-                with open(_settings_file_location_, mode='r') as settingsFile:
+                with open(_settings_file_location, mode='r') as settingsFile:
                     settingsData = settingsFile.read()
                     _settings = json.loads(settingsData)
                     settingsFile.close()
@@ -98,7 +104,7 @@ class Startup_handler(APIHandler):
             _settings['tmploc'] = real_embl_path
 
             try:
-                with open(_settings_file_location_, mode='w') as settingsFile:
+                with open(_settings_file_location, mode='w') as settingsFile:
                     settingsFile.write(json.dumps(_settings))
                     settingsFile.close()
             except Exception as ex:
@@ -112,20 +118,20 @@ class Startup_handler(APIHandler):
             logger.info("Setting PYTHONPATH environmental variable.")
             os.environ['PYTHONPATH']=f'{os.path.abspath(embl_path)}{os.pathsep}$PYTHONPATH'
             try:
-                if not os.path.exists(_settings_file_location_):
-                    if not os.path.exists(_settings_file_dir_):
+                if not os.path.exists(_settings_file_location):
+                    if not os.path.exists(_private_dir):
                         logger.info(f'Creating parent directory for settings file: {_settings_file_dir_}')                   
                         try:
-                            os.makedirs(_settings_file_dir_)
+                            os.makedirs(_private_dir)
                             logger.info(f'{_settings_file_dir_} created.') 
                         except Exception as ex:
                             logger.error(f'Failed to create parent directory for settings file at: {_settings_file_dir_}\n{ex}')
-                    logger.info(f'Settings file not found at: {_settings_file_location_}\nCreating empty settings file.')
-                    with open(_settings_file_location_, mode='w') as settingsFile:
+                    logger.info(f'Settings file not found at: {_settings_file_location}\nCreating empty settings file.')
+                    with open(_settings_file_location, mode='w') as settingsFile:
                         settingsFile.write(json.dumps({}))
                         settingsFile.close()
                         
-                with open(_settings_file_location_, mode='r') as settingsFile:
+                with open(_settings_file_location, mode='r') as settingsFile:
                     settingsData = settingsFile.read()
                     _settings = json.loads(settingsData)
                     old_location = None if 'tmploc' not in _settings else _settings['tmploc']
@@ -182,27 +188,27 @@ class settingsHandler(APIHandler):
             data = self.get_json_body()
             emailSetting = data['email']
             outdirSetting = data['outdir']
-            if not os.path.exists(_settings_file_location_):
-                logger.info(f'Settings file not found at: {_settings_file_location_}\nCreating empty settings file.')
-                if not os.path.exists(_settings_file_dir_):
+            if not os.path.exists(_settings_file_location):
+                logger.info(f'Settings file not found at: {_settings_file_location}\nCreating empty settings file.')
+                if not os.path.exists(_private_dir):
                     logger.info(f'Creating parent directory for settings file: {_settings_file_dir_}')                   
                     try:
-                        os.makedirs(_settings_file_dir_)
+                        os.makedirs(_private_dir)
                         logger.info(f'{_settings_file_dir_} created.') 
                     except Exception as ex:
                         logger.error(f'Failed to create parent directory for settings file at: {_settings_file_dir_}\n{ex}')
                 
-                with open(_settings_file_location_, mode='w') as settingsFile:
+                with open(_settings_file_location, mode='w') as settingsFile:
                     settingsFile.write(json.dumps({}))
                     settingsFile.close()
 
-            with open(_settings_file_location_, mode='r') as settingsFile:
+            with open(_settings_file_location, mode='r') as settingsFile:
                 settingsData = settingsFile.read()
                 _settings = json.loads(settingsData)
                 settingsFile.close()
             _settings['email'] = emailSetting
             _settings['outdir'] = outdirSetting
-            with open(_settings_file_location_, mode='w') as settingsFile:
+            with open(_settings_file_location, mode='w') as settingsFile:
                 settingsFile.write(json.dumps(_settings))
                 settingsFile.close()
             self.finish(json.dumps({
@@ -220,7 +226,7 @@ class ToolDescriptionsHandler(APIHandler):
 
     @tornado.web.authenticated
     def get(self):
-        desc_file_path = os.path.join(__location__, 'toolDescriptions.json')
+        desc_file_path = os.path.join(_location, 'toolDescriptions.json')
         descriptions = '{}'
         success = False
         error_msg = ''
